63292604651570662573829a38df34a50f8a9e55
thedaruma <rgiorda1@gmail.com>
2019-09-28 22:29:57 -0700

    add testing for redux.  Redux itself.

--- src/Pages/Debug/Debug.tsx
+++ src/Pages/Debug/Debug.tsx
@@ -2,8 +2,53 @@
 import React from 'react';
 
 
-export default class Debug extends React.Component {
+export class Debug extends React.Component {
   render(){
     return (<>Debug mode</>)
   }
+}
+
+
+const CounterOutput = (props) => (
+    <div className="CounterOutput">
+        Current Counter: {props.value}
+    </div>
+);
+
+
+const CounterControl = (props) => (
+    <div className="CounterControl" onClick={props.clicked}>
+        {props.label}
+    </div>
+);
+
+
+
+export class Counter extends React.Component<any, any>{
+    state = {
+        counter: 0
+    }
+
+    counterChangedHandler = ( action, value ) => {
+        switch ( action ) {
+            case 'add':
+                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
+                break;
+            case 'sub':
+                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
+                break;
+        }
+    }
+
+    render () {
+        return (
+            <div>
+                <CounterOutput value={this.state.counter} />
+                <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'add', 1 )} />
+                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'sub', 1 )}  />
+                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
+                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
+            </div>
+        );
+    }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import {createStore} from 'redux';
+import reducer from './Store/reducer';
+
+const store = createStore(reducer);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
